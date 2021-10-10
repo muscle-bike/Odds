ac8c32e6044348c6a3a666b8783a0a21e309170b
ssv <ssv@localhost>
2021-10-10 15:17:46 +0900

    HTML上のmonthで年月を取得する機能追加

--- src/main/java/com/portfolio/file/controller/HomeController.java
+++ src/main/java/com/portfolio/file/controller/HomeController.java
@@ -3,12 +3,14 @@ package com.portfolio.file.controller;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.portfolio.file.model.RacingModel;
 import com.portfolio.file.model.impl.UserDetailsImpl;
@@ -21,9 +23,6 @@ public class HomeController {
     private RacingService racingService;
     @Autowired
     private RacingRepository racingRepository;
-
-//    private int year;
-//    private int month;
 
 
  // Getメソッド
@@ -46,24 +45,26 @@ public class HomeController {
     }
 
     @GetMapping(value = "/date")
-    public String racingdate(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        this.year = year;
-//        this.year = year;
+    public String racingdate(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam(defaultValue = "NOT PARAM") String requestMonth) {
+        String[] requestMonths = requestMonth.split("-");
+        int[] yearMonth = Stream.of(requestMonths).mapToInt(Integer::parseInt).toArray();
 
         String loginUser = userDetails.getUsername();
 
 
         Calendar stertdate1 = Calendar.getInstance();
-        stertdate1.set(Calendar.YEAR, 2021);
-        stertdate1.set(Calendar.MONTH, 10 - 1);
+        stertdate1.set(Calendar.YEAR, yearMonth[0]);
+        stertdate1.set(Calendar.MONTH, yearMonth[1] - 1 );
         stertdate1.set(Calendar.DATE,    1 - 1);
         Date stertdate = new Date();
         stertdate = stertdate1.getTime();
 
         Calendar enddate1 = Calendar.getInstance();
-        enddate1.set(Calendar.YEAR, 2021);
-        enddate1.set(Calendar.MONTH,10);
-        enddate1.set(Calendar.DATE,    30);
+        enddate1.set(Calendar.YEAR, yearMonth[0]);
+        enddate1.set(Calendar.MONTH,yearMonth[1] - 1);
+        int result = enddate1.getActualMaximum(Calendar.DAY_OF_MONTH);
+        enddate1.set(Calendar.DATE, result);
         Date enddate = new Date();
         enddate = enddate1.getTime();
 
