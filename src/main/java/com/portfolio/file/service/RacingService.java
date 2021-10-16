b24d50e1011d81798b28b022f43adb5c6505eae8
ssv <ssv@192.168.100.103>
2021-10-16 22:14:00 +0900

    インデント修正

--- src/main/java/com/portfolio/file/service/RacingService.java
+++ src/main/java/com/portfolio/file/service/RacingService.java
@@ -1,4 +1,5 @@
 package com.portfolio.file.service;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,54 +14,55 @@ import com.portfolio.file.repository.RacingRepository;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class RacingService {
-@Autowired
-private RacingRepository racingRepository;
+    @Autowired
+    private RacingRepository racingRepository;
 
-//レース情報全検索
-public List<RacingModel> seachAll() {
-    return racingRepository.findAll();
-}
-//ユーザー名検索
-public List<RacingModel> findByUsername(String username){
-    return racingRepository.findByUsername(username);
-}
+    //レース情報全検索
+    public List<RacingModel> seachAll() {
+        return racingRepository.findAll();
+    }
 
-//レース情報 ID(主キー)検索
-public  RacingModel findById(Integer id) {
-    return racingRepository.getOneData(id);
-}
+    //ユーザー名検索
+    public List<RacingModel> findByUsername(String username) {
+        return racingRepository.findByUsername(username);
+    }
 
-//レース情報登録
-public void create(RacingRequest racingRequest, String loginUser) {
-    RacingModel racing_infos = new RacingModel();
-    racing_infos.setDate(racingRequest.getDate());
-    racing_infos.setRacing_name(racingRequest.getRacing_name());
-    racing_infos.setRacing_place(racingRequest.getRacing_place());
-    racing_infos.setExpenditure(racingRequest.getExpenditure());
-    racing_infos.setIncome_amount(racingRequest.getIncome_amount());
-    racing_infos.setUsername(loginUser);
-    racingRepository.save(racing_infos);
- }
+    //レース情報 ID(主キー)検索
+    public RacingModel findById(Integer id) {
+        return racingRepository.getOneData(id);
+    }
 
-//レース情報更新
-public void update(RacingUpdateRequest racingUpdateRequest) {
-    RacingModel racing_infos = findById(racingUpdateRequest.getId());
-    racing_infos.setRacing_name(racingUpdateRequest.getRacing_name());
-    racing_infos.setRacing_place(racingUpdateRequest.getRacing_place());
-    racing_infos.setExpenditure(racingUpdateRequest.getExpenditure());
-    racing_infos.setIncome_amount(racingUpdateRequest.getIncome_amount());
-    racingRepository.save(racing_infos);
-}
+    //レース情報登録
+    public void create(RacingRequest racingRequest, String loginUser) {
+        RacingModel racing_infos = new RacingModel();
+        racing_infos.setDate(racingRequest.getDate());
+        racing_infos.setRacing_name(racingRequest.getRacing_name());
+        racing_infos.setRacing_place(racingRequest.getRacing_place());
+        racing_infos.setExpenditure(racingRequest.getExpenditure());
+        racing_infos.setIncome_amount(racingRequest.getIncome_amount());
+        racing_infos.setUsername(loginUser);
+        racingRepository.save(racing_infos);
+    }
 
-//レース情報削除
-public void delete(Integer id) {
-    RacingModel racing_infos = findById(id);
-    racingRepository.delete(racing_infos);
-  }
+    //レース情報更新
+    public void update(RacingUpdateRequest racingUpdateRequest) {
+        RacingModel racing_infos = findById(racingUpdateRequest.getId());
+        racing_infos.setRacing_name(racingUpdateRequest.getRacing_name());
+        racing_infos.setRacing_place(racingUpdateRequest.getRacing_place());
+        racing_infos.setExpenditure(racingUpdateRequest.getExpenditure());
+        racing_infos.setIncome_amount(racingUpdateRequest.getIncome_amount());
+        racingRepository.save(racing_infos);
+    }
 
-//racing_infosのusernameを取得
-public void view( String operationUser) {
-    RacingModel racing_infos = new RacingModel();
-    racing_infos.setUsername(operationUser);
- }
+    //レース情報削除
+    public void delete(Integer id) {
+        RacingModel racing_infos = findById(id);
+        racingRepository.delete(racing_infos);
+    }
+
+    //racing_infosのusernameを取得
+    public void view(String operationUser) {
+        RacingModel racing_infos = new RacingModel();
+        racing_infos.setUsername(operationUser);
+    }
 }
